0517586011ad0e8b7c6bee052df6dacef01dd4ee
cNoveron <carlos.escom.ipn@gmail.com>
2022-03-10 17:05:10 -0600

    contracts: Bank is CompoundInterest

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.8.0;
 
 import "./IBank.sol";
+import "./Interest.sol";
 
-contract Bank is IBank {
+contract Bank is IBank, CompoundInterest {
 
     function deposit(uint256 amount) external payable returns (bool);
 
