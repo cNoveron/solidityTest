79b515f5678e47ea80bc1df3e04f4f9590a7a960
cNoveron <carlos.escom.ipn@gmail.com>
2022-03-10 16:22:43 -0600

    contracts/Bank: create

---
+++ contracts/Bank.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.0;
+
+import "./IBank.sol";
+
+contract Bank is IBank {
+
+    function deposit(uint256 amount) external payable returns (bool);
+
+    function withdraw(uint256 amount) external returns (uint256);
+
+    function getBalance() external view returns (uint256);
+}
