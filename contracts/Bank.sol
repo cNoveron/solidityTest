4bdeef0f3db5bdbd23f77166a02b263a1f962b83
cNoveron <carlos.escom.ipn@gmail.com>
2022-03-10 19:23:27 -0600

    fix balance calculation

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -4,6 +4,7 @@ import "./IBank.sol";
 import "./Interest.sol";
 
 contract Bank is IBank, CompoundInterest {
+    using SafeMath for uint256;
 
     function deposit(uint256 amount) override external payable returns (bool) {
         stake(amount);
@@ -16,10 +17,10 @@ contract Bank is IBank, CompoundInterest {
         if (amount == 0) amountToWithdraw = balanceOf(msg.sender);
         exit(amountToWithdraw);
         emit Withdraw(msg.sender, amount);
-        return earnedByAmount(msg.sender, amountToWithdraw);
+        return amount.add(earnedByAmount(msg.sender, amountToWithdraw));
     }
 
     function getBalance() override external view returns (uint256) {
-        return earned(msg.sender);
+        return balanceOf(msg.sender).add(earned(msg.sender));
     }
 }
